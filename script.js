c168c3fc06436333c3f8b4109510b860dfe94ca9
IbrahimAlneshmi <139479810+IbrahimAlneshmi@users.noreply.github.com>
2023-10-04 00:10:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -91,8 +91,8 @@ function pasteFromClipboard() {
 function highlightMatches() {
     let searchTerm = searchInput.value.trim().toLowerCase();
 	
-	searchTerm = searchTerm.replace(/[اأ]/g, '[أا]');
-	searchTerm = searchTerm.replace(/[إا]/g, '[اإ]');
+	searchTerm = searchTerm.replace(/[أ]/g, 'ا');
+	searchTerm = searchTerm.replace(/[إ]/g, '[ا]');
 searchTerm = searchTerm.replace(/[.]/g, '');
 searchTerm = searchTerm.replace(/[،]/g, ' ');
     // حذف المسافات الزائدة بين الكلمات (أكثر من مسافتين)
