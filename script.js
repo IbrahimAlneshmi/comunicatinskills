3b64ba3b01a0cb32d60b113d8182eb8d3c027103
IbrahimAlneshmi <139479810+IbrahimAlneshmi@users.noreply.github.com>
2023-10-23 00:27:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -114,8 +114,8 @@ function highlightMatches() {
     let searchTerm = searchInput.value.trim().toLowerCase();
 	
 	searchTerm = searchTerm.replace(/["]/g, '');
-	searchTerm = searchTerm.replace(/[:]/g, ' :');
-	searchTerm = searchTerm.replace(/[()]/g, ' ()');
+	searchTerm = searchTerm.replace(/[:]/g, '[ :]');
+	searchTerm = searchTerm.replace(/[()]/g, '[ ()]');
 	searchTerm = searchTerm.replace(/[-]/g, ' -');
 	//searchTerm = searchTerm.replace(/[و ]/g, '[و و]');//تحتاج اعادة نظر
 	    searchTerm = searchTerm.replace(/[اأإآ]/g, '[اأإآ]'); // استبدال الأحرف الممكنة بأي حرف منها
