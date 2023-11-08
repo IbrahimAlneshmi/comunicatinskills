b97b5ba6941e768c63047d0729774ec49b63b8a6
IbrahimAlneshmi <139479810+IbrahimAlneshmi@users.noreply.github.com>
2023-11-08 19:46:48 +0300

    Update scriptquizcomp.js

--- scriptquizcomp.js
+++ scriptquizcomp.js
@@ -1,1 +1,23 @@
 
+pasteButton.addEventListener('click', pasteFromClipboard);
+
+searchInput.addEventListener('input', highlightMatches);
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'v') {
+		//event.preventDefault();
+
+	    searchInput.value = '';
+        pasteFromClipboard();
+    }
+});
+
+
+
+// تعطيل اختصارات لوحة المفاتيح للنسخ
+contentElements.forEach(element => {
+    element.addEventListener('copy', function (event) {
+        event.preventDefault();
+        alert('نسخ النص غير مسموح به.');
+    });
+});
+
